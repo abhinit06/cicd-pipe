63650e5554df9f475f7516f2b34e626af817dee3
Abhishek <abhinit06@gmail.com>
2019-11-11 14:57:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def generate_buzz(a,b):
     page = '<html><body><h1>'
-    page += generator.sample(a,b)
+    page += generator.generate_buzz(3,4)
     page += '</h1></body></html>'
     return page
 
