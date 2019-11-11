3cc208153429e7cfef4f4bd1155359e68970b102
Abhishek <abhinit06@gmail.com>
2019-11-11 12:27:51 +0530

    Update test_generator.py

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -6,3 +6,8 @@ def test_sample_sum():
     sum = generator.sample(2,3)
     sum1=(5,7,9)
     assert sum in sum1
+
+def test_sample_sum1():
+    sum = generator.sample(4,5)
+    sum1=(5,7,9)
+    assert sum in sum1
